f56438d742a4f0052d59483986fe641f46931b45
Renan Storrer <storrerrenan@gmail.com>
2022-06-28 02:10:02 -0300

    requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -80,7 +80,7 @@ function catAndMouse(mouse, cat1, cat2) {
     gatoMaisPerto = 'cat1';
   } else if (distanceCat1 > distanceCat2) {
     gatoMaisPerto = 'cat2';
-  } else if (distanceCat1 == distanceCat2){
+  } else if (distanceCat1 == distanceCat2) {
     gatoMaisPerto = 'os gatos trombam e o rato foge';
   }
   return gatoMaisPerto
@@ -92,11 +92,11 @@ console.log(catAndMouse(0, 2, 1));
 function fizzBuzz(numerosFizzBuzz) {
   let resultadoFizzBuzz = [];
   for (let index in numerosFizzBuzz) {
-    if (numerosFizzBuzz[index] % 3  === 0 && numerosFizzBuzz[index] % 5 !== 0) {
+    if (numerosFizzBuzz[index] % 3 === 0 && numerosFizzBuzz[index] % 5 !== 0) {
       resultadoFizzBuzz.push('fizz')
     } else if (numerosFizzBuzz[index] % 5 === 0 && numerosFizzBuzz[index] % 3 !== 0) {
       resultadoFizzBuzz.push('buzz')
-    } else if (numerosFizzBuzz[index] % 3 === 0 && numerosFizzBuzz[index] % 5 ===0) {
+    } else if (numerosFizzBuzz[index] % 3 === 0 && numerosFizzBuzz[index] % 5 === 0) {
       resultadoFizzBuzz.push('fizzBuzz')
     } else {
       resultadoFizzBuzz.push('bug!')
@@ -107,12 +107,52 @@ function fizzBuzz(numerosFizzBuzz) {
 console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
-function encode() {
-  
-};
-function decode() {
-  // seu código aqui
+function encode(stringEncode) {
+  let stringInArray = stringEncode.split('');
+  let arrayEncode = [];
+  for (let index in stringInArray) {
+    if(stringInArray[index] === 'a') {
+      arrayEncode.push('1')
+    } else if (stringInArray[index] === 'e') {
+      arrayEncode.push('2')
+    } else if (stringInArray[index] === 'i') {
+      arrayEncode.push('3')
+    } else if (stringInArray[index] === 'o') {
+      arrayEncode.push('4')
+    } else if (stringInArray[index] === 'u') {
+      arrayEncode.push('5')
+    } else {
+      arrayEncode.push(stringInArray[index])
+    }
+  }
+  let resultEncode = arrayEncode.join('')
+  return resultEncode
+
 }
+console.log(encode('hi there!'))
+
+function decode(stringDecode) {
+  let stringInArray2 = stringDecode.split('');
+  let arrayDecode = [];
+  for (let index in stringDecode) {
+    if(stringInArray2[index] === '1') {
+      arrayDecode.push('a')
+    } else if (stringInArray2[index] === '2') {
+      arrayDecode.push('e')
+    } else if (stringInArray2[index] === '3') {
+      arrayDecode.push('i')
+    } else if (stringInArray2[index] === '4') {
+      arrayDecode.push('o')
+    } else if (stringInArray2[index] === '5') {
+      arrayDecode.push('u')
+    } else {
+      arrayDecode.push(stringInArray2[index])
+    }
+  }
+  let resultadoDecode = arrayDecode.join('');
+  return resultadoDecode
+}
+console.log(decode('h3 th2r2!'))
 
 // Desafio 10
 function techList() {
