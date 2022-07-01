23cbfdcf1762d20c84cef0fe60a56289e8092069
Renan Storrer <storrerrenan@gmail.com>
2022-07-01 19:50:04 -0300

    REQUISITO 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,33 +1,51 @@
 // Desafio 11
-function generatePhoneNumber(numberPhone) {
-  let ddd = '(' + numberPhone[0] + numberPhone[1] + ')'
-  let primeirosNumeros = [];
-  for (let index = 2; index < 7; index += 1){
-    primeirosNumeros.push(numberPhone[index])
+function generatePhoneNumber(numbers) {
+  let phone = []
+
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.'
+  } else {
+    for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+      let check = 0;
+      for (let index3 = 0; index3 < numbers.length; index3 += 1) {
+        if (index3 == index2) {
+          check = check + 1
+        }
+      }
+      if (index2 < 0 || index2 > 9 || check >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores'
+      }
+    }
+    for (let index4 = 0; index4 < numbers.length; index += 1) {
+      if (index4 === 0) {
+        phone += '(' + numbers[index4]
+      } else if (index4 === 1) {
+        phone += numbers[index4] + ')'
+      } else if (index4 === 6) {
+        phone += numbers[index4] + '-'
+      } else {
+        phone += numbers[index4]
+      }
+    }
   }
-  let primeirosNumerosString = primeirosNumeros.join('');
-  let ultimosNumeros = [];
-  for (let index2 = 7; index2 < numberPhone.length; index2 +=1) {
-    ultimosNumeros.push(numberPhone[index2])
-  }
-  let ultimosNumerosString = ultimosNumeros.join('');
-  let numeroFinal = ddd + ' ' + primeirosNumerosString + '-' + ultimosNumerosString;
-  let mensagemFinal;
-  if (numeroFinal.length === 15) {
-    mensagemFinal = numeroFinal
-  } else  if (numeroFinal.length != 15) {
-    mensagemFinal = 'Array com tamanho incorreto.' 
-  }
-  // console.log(mensagemFinal);
-  return mensagemFinal
+  return phone
 }
-generatePhoneNumber([4, 1, 9, 9, 5, 8, 4, 3, 2, 1, 2, ]);
+
+
+generatePhoneNumber([4, 1, 9, 9, 5, 8, 4, 3, 2, 1, 2]);
 
 // Desafio 12
-function triangleCheck(lineA, lineB, lineC)  {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+  // let lines = [lineA, lineB, lineC]
+  // let triangle;
+  // for (let index = 0; index < lines.length; index = 0){
+  //   let line = lines[index]
+  //   for (let index2 = 0; index < lines.length; index += 1) {
 
+  //   }
+  // }
+}
+triangleCheck()
 // Desafio 13
 function hydrate() {
   // seu código aqui
