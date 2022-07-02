6fbb0dda0862fb0f82e977ffb3572165638b249f
Renan Storrer <storrerrenan@gmail.com>
2022-07-02 03:54:43 -0300

    requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,38 +1,35 @@
 // Desafio 11
 function generatePhoneNumber(numbers) {
-  let phone = []
+  let phone = '(';
 
-  if (numbers.length !== 11) {
+  if (numbers.length != 11) {
     return 'Array com tamanho incorreto.'
   } else {
-    for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+    for (number of numbers) {
       let check = 0;
-      for (let index3 = 0; index3 < numbers.length; index3 += 1) {
-        if (index3 == index2) {
-          check = check + 1
+      for (let checkNumber = 0; checkNumber < numbers.length; checkNumber += 1) {
+        if (checkNumber == number) {
+          check += 1
         }
       }
-      if (index2 < 0 || index2 > 9 || check >= 3) {
+      if (number < 0 || number > 9 || check >= 3) {
         return 'não é possível gerar um número de telefone com esses valores'
       }
     }
-    for (let index4 = 0; index4 < numbers.length; index += 1) {
-      if (index4 === 0) {
-        phone += '(' + numbers[index4]
-      } else if (index4 === 1) {
-        phone += numbers[index4] + ')'
-      } else if (index4 === 6) {
-        phone += numbers[index4] + '-'
+    for (let index = 0; index < numbers.length; index += 1) {
+      if (index == 1) {
+        phone += numbers[index] + ') ';
+      } else if (index === 6) {
+        phone += numbers[index] + '-'
       } else {
-        phone += numbers[index4]
+        phone += numbers[index]
       }
     }
+    return phone
   }
-  return phone
 }
 
-
-generatePhoneNumber([4, 1, 9, 9, 5, 8, 4, 3, 2, 1, 2]);
+// generatePhoneNumber([4, 1, 9, 9, 5, 8, 4, 3, 2, 1, 2]);
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
