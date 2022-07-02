33dcd3558933d789dec04edd78150f4175663f86
Renan Storrer <storrerrenan@gmail.com>
2022-07-02 04:17:44 -0300

    requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,45 +1,48 @@
 // Desafio 11
 function generatePhoneNumber(numbers) {
-  let phone = '('
-  if(numbers.length !== 11){
+  let phone = '(';
+  if (numbers.length !== 11) {
     return 'Array com tamanho incorreto.'
   } else {
-    for(let index = 0; index < numbers.length; index += 1){
+    for (number of numbers) {
       let contagem = 0;
-      for (let index2 = 0; index2 < numbers.length; index2 += 1){
-        if (index2 === index){
+      for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+        if (index2 === number) {
           contagem += 1;
         }
       }
-      if (index < 0 || index > 9 || contagem > 2) {
+      if (number < 0 || number > 9 || contagem >= 3) {
         return 'não é possível gerar um número de telefone com esses valores'
       }
-    }
-    for (let index3 = 0; index3 < numbers.length; index3 += 1){
-      if (index3 === 1) {
-        phone += numbers[index3] = ') '
-      } else if (index3 === 6) {
-        phone += numbers[index3]
-      } else {
-        phone += numbers[index3]
+      for (let index3 = 0; index3 < numbers.length; index3 += 1) {
+        if (index3 === 1) {
+          phone += numbers[index3] + ') ';
+        } else if (index3 === 6) {
+          phone += numbers[index3] + '-';
+        } else {
+          phone += numbers[index3];
+        }
       }
+      return phone;
     }
-    return phone
   }
 }
+
+// generatePhoneNumber(numbers);
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  // let lines = [lineA, lineB, lineC]
-  // let triangle;
-  // for (let index = 0; index < lines.length; index = 0){
-  //   let line = lines[index]
-  //   for (let index2 = 0; index < lines.length; index += 1) {
-
-  //   }
-  // }
+  // seu código aqui
+  if (lineA < (lineB + lineC) && lineB < (lineA + lineC) && lineC < (lineA + lineB)) {
+    if (lineA > (Math.abs(lineB - lineC)) && lineB > (Math.abs(lineA - lineC)) && lineC > (Math.abs(lineA - lineB))) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 }
-triangleCheck()
-// Desafio 13
+
 function hydrate() {
   // seu código aqui
 }
