e68be6ff801fa36351ac0e07d800e6bb0e532c50
Renan Storrer <storrerrenan@gmail.com>
2022-07-02 04:22:06 -0300

    refazer requisito 11 desde o inicio

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,31 +1,5 @@
 // Desafio 11
 function generatePhoneNumber(numbers) {
-  let phone = '(';
-  if (numbers.length !== 11) {
-    return 'Array com tamanho incorreto.'
-  } else {
-    for (number of numbers) {
-      let contagem = 0;
-      for (let index2 = 0; index2 < numbers.length; index2 += 1) {
-        if (index2 === number) {
-          contagem += 1;
-        }
-      }
-      if (number < 0 || number > 9 || contagem >= 3) {
-        return 'não é possível gerar um número de telefone com esses valores'
-      }
-      for (let index3 = 0; index3 < numbers.length; index3 += 1) {
-        if (index3 === 1) {
-          phone += numbers[index3] + ') ';
-        } else if (index3 === 6) {
-          phone += numbers[index3] + '-';
-        } else {
-          phone += numbers[index3];
-        }
-      }
-      return phone;
-    }
-  }
 }
 
 // generatePhoneNumber(numbers);
