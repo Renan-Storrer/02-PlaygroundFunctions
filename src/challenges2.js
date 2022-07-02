bbb3325923ce6cf5883fa3276414ddd1fff69309
Renan Storrer <storrerrenan@gmail.com>
2022-07-02 04:04:52 -0300

    requisito 11 aaaaaaaaaaaaaaaaaaaaaaaa

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,36 +1,32 @@
 // Desafio 11
 function generatePhoneNumber(numbers) {
-  let phone = '(';
-
-  if (numbers.length != 11) {
+  let phone = '('
+  if(numbers.length !== 11){
     return 'Array com tamanho incorreto.'
   } else {
-    for (number of numbers) {
-      let check = 0;
-      for (let checkNumber = 0; checkNumber < numbers.length; checkNumber += 1) {
-        if (checkNumber == number) {
-          check += 1
+    for(let index = 0; index < numbers.length; index += 1){
+      let contagem = 0;
+      for (let index2 = 0; index2 < numbers.length; index2 += 1){
+        if (index2 === index){
+          contagem += 1;
         }
       }
-      if (number < 0 || number > 9 || check >= 3) {
+      if (index < 0 || index > 9 || contagem > 2) {
         return 'não é possível gerar um número de telefone com esses valores'
       }
     }
-    for (let index = 0; index < numbers.length; index += 1) {
-      if (index == 1) {
-        phone += numbers[index] + ') ';
-      } else if (index === 6) {
-        phone += numbers[index] + '-'
+    for (let index3 = 0; index3 < numbers.length; index3 += 1){
+      if (index3 === 1) {
+        phone += numbers[index3] = ') '
+      } else if (index3 === 6) {
+        phone += numbers[index3]
       } else {
-        phone += numbers[index]
+        phone += numbers[index3]
       }
     }
     return phone
   }
 }
-
-// generatePhoneNumber([4, 1, 9, 9, 5, 8, 4, 3, 2, 1, 2]);
-
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   // let lines = [lineA, lineB, lineC]
